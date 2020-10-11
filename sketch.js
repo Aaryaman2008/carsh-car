2551bfd76f3b034f2a6691b9473a872f889e2450
Aaryaman2008 <71973305+Aaryaman2008@users.noreply.github.com>
2020-10-11 17:03:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ if(wall.x-car.x< (car.width+wall.width)/2){
 car.velocityX=0;
 var deformation=0.5 * speed * weight *speed/22509
 
-if(car.velocityX >180  ){
+if(deformation>180  ){
 
 car.shapeColor="red";
 
